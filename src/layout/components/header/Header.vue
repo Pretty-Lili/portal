9f3922b679e0e6f98c2b8fa99c1c9e6708c08477
xueyali <1689651719@qq.com>
2019-09-05 16:26:39 +0800

    init commit

--- src/layout/components/header/Header.vue
+++ src/layout/components/header/Header.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <top-bar></top-bar>
+    <!-- <menu-con></menu-con> -->
     <page-header></page-header>
   </div>
 </template>
 <script>
 import TopBar from './TopBar'
 import PageHeader from './PageHeader'
+import MenuCon from './Menu'
 export default {
   name: 'HeaderContainer',
   components: {
     TopBar,
-    PageHeader
+    PageHeader,
+    MenuCon
   }
 }
 </script>
